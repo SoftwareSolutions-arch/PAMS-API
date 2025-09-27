e88b5b25c2c5b39a31cf2bc8ff9ef88a8edc0d6b
Umang Chopra <60170659+SoftwareSolutions-arch@users.noreply.github.com>
2025-09-27 20:41:45 +0530

    email service

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,14 +1,5 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
 
 /**
  * Send an email
@@ -17,23 +8,32 @@ const transporter = nodemailer.createTransport({
  * @param {string} html - html content
  */
 export const sendEmail = async (to, subject, html) => {
-  try {
-    if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-      throw new Error("❌ Missing EMAIL_USER or EMAIL_PASS in .env");
+    try {
+        if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
+            throw new Error("❌ Missing EMAIL_USER or EMAIL_PASS in .env");
+        }
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            },
+        });
+
+        const mailOptions = {
+            from: `"PAMS Support" <${process.env.EMAIL_USER}>`,
+            to,
+            subject,
+            html,
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log("✅ Email sent:", info.messageId);
+        return info;
+    } catch (error) {
+        console.error("❌ Email send failed:", error);
+        throw new Error("Email could not be sent");
     }
-
-    const mailOptions = {
-      from: `"PAMS Support" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    };
-
-    const info = await transporter.sendMail(mailOptions);
-    console.log("✅ Email sent:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("❌ Email send failed:", error);
-    throw new Error("Email could not be sent");
-  }
 };
